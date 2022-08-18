bb26c91e5a55b09496653662ac9090ab5ad13756
Gustavo Mathias <gmgustavom@gmail.com>
2022-08-18 09:28:20 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>Trybeflix</div>
+  );
+}
+
+export default App;
---
+++ src/data/movies.js
@@ -0,0 +1,67 @@
+export const movieList = [
+  {
+    movieName: 'Carros',
+    releaseYear: 2006,
+    image: 'https://upload.wikimedia.org/wikipedia/en/3/34/Cars_2006.jpg',
+  },
+  {
+    movieName: 'Toy Story',
+    releaseYear: 1995,
+    image: 'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+  },
+  {
+    movieName: 'Up! Altas aventuras',
+    releaseYear: 2009,
+    image: 'https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg',
+  },
+  {
+    movieName: 'Lightyear',
+    releaseYear: 2022,
+    image: 'https://upload.wikimedia.org/wikipedia/en/f/f9/Lightyear_%28film%29_poster.jpg',
+  },
+  {
+    movieName: 'Red: Crescer é uma fera',
+    releaseYear: 2022,
+    image: 'https://upload.wikimedia.org/wikipedia/en/9/9e/Turning_Red_poster.jpg',
+  },
+  {
+    movieName: 'Monstros S.A',
+    releaseYear: 2001,
+    image: 'https://upload.wikimedia.org/wikipedia/pt/7/75/Monsters_Inc.jpg',
+  },
+  {
+    movieName: 'Divertidamente',
+    releaseYear: 2015,
+    image: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg',
+  },
+  {
+    movieName: 'Os Incriveis',
+    releaseYear: 2004,
+    image: 'https://upload.wikimedia.org/wikipedia/en/2/27/The_Incredibles_%282004_animated_feature_film%29.jpg',
+  },
+  {
+    movieName: 'WALL-E',
+    releaseYear: 2008,
+    image: 'https://upload.wikimedia.org/wikipedia/pt/3/30/WALL-E.jpg',
+  },
+  {
+    movieName: 'Procurando Nemo',
+    releaseYear: 2003,
+    image: 'https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg',
+  },
+  {
+    movieName: 'Ratatouille',
+    releaseYear: 2007,
+    image: 'https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg',
+  },
+  {
+    movieName: 'Soul',
+    releaseYear: 2020,
+    image: 'https://upload.wikimedia.org/wikipedia/en/3/39/Soul_%282020_film%29_poster.jpg',
+  },
+  {
+    movieName: 'Vida de inseto',
+    releaseYear: 1998,
+    image: 'https://upload.wikimedia.org/wikipedia/en/c/cc/A_Bug%27s_Life.jpg',
+  }
+]
